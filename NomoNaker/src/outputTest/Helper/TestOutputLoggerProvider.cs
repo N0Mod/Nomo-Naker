2174ed6d8b72c7282c94093169ee4b4bd94067b6
Nomod <konlagheshon@proton.me>
2025-07-09 20:32:47 +0330

    Delete NomoNaker/src directory

--- NomoNaker/src/outputTest/Helper/TestOutputLoggerProvider.cs
+++
@@ -1,8 +0,0 @@
-namespace Downloader.Test.Helper;
-
-public class TestOutputLoggerProvider(ITestOutputHelper outputHelper) : ILoggerProvider
-{
-    public ILogger CreateLogger(string categoryName) => new TestOutputLogger(outputHelper, categoryName);
-
-    public void Dispose() { }
-}
